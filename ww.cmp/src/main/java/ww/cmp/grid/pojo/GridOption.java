6d808c0f874c55a039a3b352f6c3f5a955e77352
leezw03 <leezw03@163.com>
2016-12-12 23:09:01 +0800

    框架搭建

--- ww.cmp/src/main/java/ww/cmp/grid/pojo/GridOption.java
+++ ww.cmp/src/main/java/ww/cmp/grid/pojo/GridOption.java
@@ -1,6 +1,39 @@
 package ww.cmp.grid.pojo;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class GridOption {
 
+	private String handler;
 	
+	private Map<String, Object> attrs;
+	
+	public GridOption() {
+		attrs = new HashMap<String, Object>();
+	}
+
+	public String getHandler() {
+		return handler;
+	}
+
+	public void setHandler(String handler) {
+		this.handler = handler;
+	}
+
+	public Map<String, Object> getAttrs() {
+		return attrs;
+	}
+
+	public void setAttrs(Map<String, Object> attrs) {
+		this.attrs = attrs;
+	}
+	
+	public Object getAttr(String name) {
+		return this.attrs.get(name);
+	}
+	
+	public void setAttr(String name, Object value) {
+		this.attrs.put(name, value);
+	}
 }
