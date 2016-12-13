bfe49a3f34d94320467d7590e174d066952c11ab
leezw03 <leezw03@163.com>
2016-12-13 18:15:19 +0800

    框架搭建

--- ww.cmp/src/main/java/ww/cmp/grid/iface/IGridBO.java
+++ ww.cmp/src/main/java/ww/cmp/grid/iface/IGridBO.java
@@ -2,15 +2,15 @@ package ww.cmp.grid.iface;
 
 import java.util.List;
 
-import ww.cmp.grid.pojo.GridOption;
+import ww.cmp.grid.pojo.GridParam;
 import ww.cmp.grid.pojo.PageResult;
 import ww.core.mvc.pojo.PageParam;
 import ww.db.mybatis.pojo.QueryParam;
 
 public interface IGridBO<T> {
 
-	public List<T> load(GridOption option, QueryParam query, PageParam page);
+	public List<T> load(GridParam param, QueryParam query, PageParam page);
 	
-	public PageResult page(GridOption option, QueryParam query, PageParam page);
+	public PageResult page(GridParam param, QueryParam query, PageParam page);
 	
 }
--- ww.cmp/src/main/java/ww/cmp/grid/pojo/GridParam.java
+++ ww.cmp/src/main/java/ww/cmp/grid/pojo/GridParam.java
@@ -1,15 +1,18 @@
 package ww.cmp.grid.pojo;
 
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
-public class GridOption {
+public class GridParam implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	private String handler;
 	
 	private Map<String, Object> attrs;
 	
-	public GridOption() {
+	public GridParam() {
 		attrs = new HashMap<String, Object>();
 	}
 
